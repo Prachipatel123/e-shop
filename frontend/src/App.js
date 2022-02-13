15c74e0b7f5264239a2301853c5efce4fe9c756e
Prachipatel123 <86521605+Prachipatel123@users.noreply.github.com>
2022-02-13 08:35:03 -0500

    basic setup ready

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import {Container} from 'react-bootstrap'; 
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return(
+  <div>
+    <Header />
+      <main>
+        <Container> 
+
+           <HomeScreen /> 
+
+        </Container> 
+      </main>
+    <Footer />
+  </div>
+  ) 
+}
+
+export default App;
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { propTypes } from 'react-bootstrap/esm/Image'
+
+
+function Products({product}) {
+  return (
+    <h5>{product.name}</h5>
+  
+  )
+}
+
+export default Products
